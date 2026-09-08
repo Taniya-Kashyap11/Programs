588f8a3235e83a6cbb8a9bd4c777c717a589aa4d
Taniya <taniyakashyap2005@gmail.com>
2026-09-08 20:00:36 +0530

    ASCII values with equivalent character

---
+++ P25.c
@@ -0,0 +1,9 @@
+/*Write a program to print all the ASCII values and their equivalent characters using a while loop. The
+ASCII values vary from 0 to 255*/
+#include<stdio.h>
+int main(){
+    for(int i=0;i<=255;i++){
+        printf("\n %d | %c",i,i);
+    }
+    return 0;
+}

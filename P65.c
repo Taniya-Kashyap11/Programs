de5deb102e0cc2054583aa827e6ed73983a21bd2
Taniya <taniyakashyap2005@gmail.com>
2026-09-13 11:50:44 +0530

    Miscellaneous Programs

---
+++ P65.c
@@ -0,0 +1,19 @@
+/*A positive integer is entered through the keyboard, write a function to calculate sum of digits of the
+number.*/
+#include<stdio.h>
+int sumOfDigits(int);
+int main(){
+    int num;
+    printf("Enter number");
+    scanf("%d",&num);
+    printf("Sum of digits is %d",sumOfDigits(num));
+    return 0;
+}
+int sumOfDigits(int n){
+    int sum=0;
+    while(n!=0){
+        sum+=(n%10);
+        n/=10;
+    }
+    return sum;
+}

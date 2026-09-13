ee5398a1fa239d206495a93592ae6a5c2272fe86
Taniya <taniyakashyap2005@gmail.com>
2026-09-13 11:21:44 +0530

    Print hello world without using semicolon

---
+++ P145.c
@@ -0,0 +1,16 @@
+/*C program to print hello world without using semicolon.*/
+#include<stdio.h>
+int main(){
+    // if(printf("Hello World"))   approach 1
+
+    // Approach 2
+    // while(!printf("Hello World")){
+
+    // }
+
+    //Approach 3
+     if(puts("Hello World"))
+    {
+    }
+    return 0;
+}
